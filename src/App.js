6cfc34c55c762e389b7a70e1c5253dbf630bf83f
AnnaCherryLittle <anna.chirka@gmail.com>
2024-01-11 12:39:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import ComponentRecipes from './Componentrecipes';
+import Buttons from './Buttons';
+
 
 function App() {
+
+  const my_id = "ab891ddf";
+  const my_key = "68192bf1bc07c12aa6b9572e65850e12";
+
+  const [search, setSearch] = useState("");
+  const [myRecipes, setMyRecipes] = useState([]);
+  const [mySubmit, setMySubmit] = useState("ice");
+  const [type, setType] = useState([]);
+
+  useEffect(() => {
+    const getRecipe = async () => {
+      const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${mySubmit}&app_id=${my_id}&app_key=${my_key}`);
+      const data = await response.json();
+      setMyRecipes(data.hits);
+      console.log(data.hits);
+    }
+    getRecipe()
+  }, [mySubmit])
+
+  const recipeScan = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const finalSearch = (e) => {
+    e.preventDefault();
+    setMySubmit(search);
+  }
+
+  // const chooseRecipe = ComponentRecipes.filter(element => 
+  //   element.recipe.dishType === dishType);
+  //   setType(type);
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <div className="container-cap">
+        <div className="container">
+        <h1>Choose a Recipe</h1>
+      </div>
+
+      <div className="container-input">
+        <form onSubmit={finalSearch}>
+          <input className="input" onChange={recipeScan} value = {search} />
+        </form>
+        <button className="btn" onClick={finalSearch}>🔍</button>
+      </div>
+      </div>
+
+      {/* <div className="container">
+        <Buttons filteredBtnRecipe={chooseRecipe}/>
+      </div> */}
+
+      <div className="container-recipes">
+      {myRecipes.map((element, index) => (
+        <ComponentRecipes key={index}
+        label={element.recipe.label} 
+        image={element.recipe.image}  
+        ingredients={element.recipe.ingredientLines}
+        calories={element.recipe.calories}
+        dishType={element.recipe.dishType}/>
+      ))}
+      </div>
     </div>
   );
 }
---
+++ src/Componentrecipes.js
@@ -0,0 +1,27 @@
+function ComponentRecipes ({label, image, calories, ingredients, dishType}) {
+    return (
+        <div className="fon">
+            <div className="container-info">
+                <h2>{label}</h2>
+            </div>
+
+            <div className="container-info">
+                <img src={image} alt="image"/>
+            </div>
+
+            <ul className="container-info list">
+                {ingredients.map((ingredient, index) => (
+                    <li key={index}>
+                        {ingredient}
+                    </li>
+                ))}
+            </ul>
+
+            <div className="container-info">
+                <p>{calories.toFixed()} calories</p>
+            </div>
+        </div>
+    )
+}
+
+export default ComponentRecipes;
